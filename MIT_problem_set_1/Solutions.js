411ea3ecbd4c79f21413f892e27f848a18e4f5ca
James Montalvo <jamesmontalvo3@gmail.com>
2014-12-08 22:01:53 -0600

    improved solutions

--- MIT_problem_set_1/Solutions.js
+++ MIT_problem_set_1/Solutions.js
@@ -1,17 +1,9 @@
 /**
- *  Algorithms.js
- *
- *
+ *  Problem 1-3(b): Give an algorithm to find the vertex with the maximum
+ *  x-coordinate in O( lg n ) time, where A[0] is the minimum x-coordinate
  */
 
-/*
-	var coords = [
-		{ x: x0, y: y0 },
-		{ x: x1, y: y1 },
-		...
-	];
-*/
-
+// example data
 var coords = [
 	{ x: 0, y: 0 },
 	{ x: 10, y: -20 },
@@ -24,32 +16,18 @@ var coords = [
 	{ x: 15, y: 15 }
 ];
 
-var count = 0;
-
 var getLargestX = function ( coords, start, end ) {
 
-	if ( count > 20 ) {
-		console.log( "too much recursion" );
-		return { x: null, y: null }
-	} else {
-		count++;
-	}
-
 	if ( end - start === 1 ) {
-		console.log( "base case" );
 		if ( coords[ start ].x > coords[ end ].x ) {
 			return coords[ start ];
 		}
 		else {
 			return coords[ end ];
 		}
-		
 	}
 
-
-
 	var mid = Math.floor( ( end + start ) / 2 );
-	console.log( "mid = " + mid );
 	
 	var firstMax  = Math.max( coords[ start ].x, coords[ mid ].x );
 	var secondMax = Math.max( coords[ mid + 1 ].x, coords[ end ].x );
@@ -63,16 +41,25 @@ var getLargestX = function ( coords, start, end ) {
 
 }
 
-getLargestX( coords, 0, coords.length - 1 );
+var test = getLargestX( coords, 0, coords.length - 1 );
+
+if ( test.x == 55 ) {
+	console.log( "getLargestX: pass" );
+}
+else {
+	console.log( "getLargestX: fail" );
+}
 
 
 
+/**
+ *  Problem 1-3(c): Give an algorithm to find the vertex with the maximum
+ *  y-coordinate in O( lg n ) time (where A[0] could be any y-coordinate)
+ */
 
-/** GET largest Y **/
 
-
-
-var testCoords1 = [
+// example data
+var testCoords = [
 	{ x: 0, y: 0 },
 	{ x: 10, y: -20 },
 	{ x: 20, y: -25 },
@@ -84,88 +71,81 @@ var testCoords1 = [
 	{ x: 15, y: 15 }
 ];
 
-var testCoords2 = [
-	{ x: 40, y: -22 },
-	{ x: 50, y: -10 },
-	{ x: 55, y: 5 },
-	{ x: 45, y: 20 },
-	{ x: 30, y: 25 },
-	{ x: 15, y: 15 },
-	{ x: 0, y: 0 },
-	{ x: 10, y: -20 },
-	{ x: 20, y: -25 }
-];
+var getLargestY = function ( coords, start, end ) {
 
-
-
-
-
-var count = 0;
-
-var getLargestX = function ( coords, start, end ) {
-
-	if ( count > 20 ) {
-		console.log( "too much recursion" );
-		return { x: null, y: null }
-	} else {
-		count++;
-	}
-	
+	// not really a "length" since subArrayLength == 2 means there are three
+	// elements
 	var subArrayLength = end - start;
+
+	// base cases where there are 2 or 3 elements in the sub-array
 	if ( subArrayLength < 3 ) {
-		console.log( "base case #1" );
 
-		var guess = coords[ start ];
+		var guess = coords[ start ]; // initially assume start is larger
 
-		if ( coords[ end ].x > guess.x ) {
+		if ( coords[ end ].y > guess.y ) {
 			guess = coords[ end ];
 		}
 
-		if ( subArrayLength == 2 && coords[ start + 1 ].x > guess.x ) {
-			guess = coords[ start + 1 ].x;
+		if ( subArrayLength == 2 && coords[ start + 1 ].y > guess.y ) {
+			guess = coords[ start + 1 ];
 		}
 
 		return guess;
 		
 	}
 
-
-	// get mid, quart1, quart3
+	// get midpoint
 	var mid = Math.floor( ( end + start ) / 2 );
-	var quart1 = getQuarterPoint( start, end );
-	var quart3 = getThreeQuarterPoint( start, end );
-
-
-	console.log(
-		"start: " + start
-		+ "\nquart1: " + quart1
-		+ "\nmid: " + mid
-		+ "\nquart3: " + quart3
-		+ "\nend: " + end
-	);
-	
-	var firstMax  = Math.max( coords[ start ].x, coords[ quart1 ].x, coords[ mid ].x );
-	var secondMax = Math.max( coords[ mid + 1 ].x, coords[ quart3 ].x, coords[ end ].x );
+		
+	// using Math.max is sort of cheating...but I would have just created my
+	// own max function which would have done the same thing.
+	var firstMax  = Math.max( coords[ start ].y, coords[ mid ].y );
+	var secondMax = Math.max( coords[ mid + 1 ].y, coords[ end ].y );
 	
 	if ( firstMax > secondMax ) {
-		return getLargestX( coords, start, mid );
+		return getLargestY( coords, start, mid );
 	}
 	else {
-		return getLargestX( coords, mid + 1, end );
+		return getLargestY( coords, mid + 1, end );
 	}
 
 };
 
-var getQuarterPoint = function ( s, e ) {
-	return s + 1;
-	//return Math.floor( ( 3 * s + e ) / 4 );
-};
 
-var getThreeQuarterPoint = function ( s, e ) {
-	return e - 1;
-	// return Math.floor( ( s + 3 * e ) / 4 );
-};
+// run the tests
+var testCoordsLast = testCoords.length - 1;
+for ( var i = 0; i < testCoords.length; i++ ) {
 
-getLargestX( testCoords2, 0, testCoords2.length - 1 );
+	var test = getLargestY( testCoords, 0, testCoordsLast );
+	if ( test.y == 25 ) {
+		console.log( "getLargestY shifted " + i + ": pass" );
+	}
+	else {
+		console.log( "getLargestY shifted " + i + ": fail" );
+		console.log( test );
+	}
+
+	// remove element from beginning of array and put it at the end
+	testCoords.push( testCoords.shift() );
+
+}
+
+testCoords.reverse();
+
+for ( var i = 0; i < testCoords.length; i++ ) {
+
+	var test = getLargestY( testCoords, 0, testCoordsLast );
+	if ( test.y == 25 ) {
+		console.log( "getLargestY reversed/shifted " + i + ": pass" );
+	}
+	else {
+		console.log( "getLargestY reversed/shifted " + i + ": fail" );
+		console.log( test );
+	}
+
+	// remove element from beginning of array and put it at the end
+	testCoords.push( testCoords.shift() );
+
+}
 
 
